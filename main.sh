970f53275c9e896e1330d90188a6d1b48a87a3fd
Aaron Schif <aaronschif@gmail.com>
2014-02-20 23:57:21 -0600

    Trunctuation.

--- main.sh
+++ main.sh
@@ -10,12 +10,13 @@ esac
 . ./history.sh
 . ./misc.sh
 
+export PROMPT_DIRTRIM=2
 if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
 color_prompt=yes
 else
 color_prompt=
 fi
-export PS1="\`__git_ps1 \"[\[${Green}\]%s\[${Reset}\]] \"\`\w \[${Yellow}\]\$\[${Reset}\] "
+export PS1="\`__git_ps1 '[\[${Green}\]%s\[${Reset}\]] '\`\u@\h\n\w \[${Yellow}\]\$\[${Reset}\] "
 
 case "$TERM" in
 xterm*|rxvt*) # Can set title
