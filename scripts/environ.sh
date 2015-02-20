7217123cbe04b289f86149f0acad4ced108966a5
Aaron Schif <aaronschif@gmail.com>
2015-02-20 14:10:19 -0600

    Reenable VIRTUAL_ENV_DISABLE_PROMPT=true

--- scripts/environ.sh
+++ scripts/environ.sh
@@ -2,4 +2,4 @@ export PROJECT_HOME="$HOME/Projects"
 export FAB_COMPLETION_FABFILE_DIR="conf"
 export ANSIBLE_NOCOWS=true
 export SUDO_PROMPT=`echo -e "${Yellow}[sudo password]${Reset} "`
-# export VIRTUAL_ENV_DISABLE_PROMPT=true
+export VIRTUAL_ENV_DISABLE_PROMPT=true
