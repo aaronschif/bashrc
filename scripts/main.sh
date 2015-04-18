0a5d2b2df25a8064058da1ee9587e715cd052797
Aaron Schif <aaronschif@gmail.com>
2015-04-18 00:06:20 -0500

    Cleanup

--- scripts/main.sh
+++ scripts/main.sh
@@ -42,18 +42,10 @@ __smart_fab_set()
 	if [ -f "conf/fabfile.py" ]
 	then
 		alias fab="fab -f conf/fabfile.py"
-		export FAB_COMPLETION_FABFILE_DIR='conf'
 	else
 		unalias fab 2> /dev/null
-		export FAB_COMPLETION_FABFILE_DIR='.'
 	fi
 }
-
-# __pwd()
-# {
-# 	i=${PWD//$HOME/'~'}
-# 	printf "${1}"${i//'/'/"${2}/${1}"}"\[${Reset}\]
-# }
 
 __color_host()
 {
@@ -75,13 +67,6 @@ venv="\`__py_virt_ps1 '${eBlue} - ${eGreen}%s '\`"
 export PS1="
 ${eBlue}- ${eGreen}\u${eBlue}${host}${git}${venv}
 ${eBlue}- ${eGreen}\w${eBlue} \$${eReset} "
-
-case "$TERM" in
-xterm*|rxvt*) # Can set title
-    ;;
-*)
-    ;;
-esac
 
 export PYENV_ROOT="$HOME/.pyenv"
 ! [[ "$PATH" =~ "$PYENV_ROOT/bin" ]] && export PATH="$PYENV_ROOT/bin:$PATH"
